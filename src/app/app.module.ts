df7b1a7a0e799e2d4bd0900a2c0158d1b29cc437
newhask777 <newhaskellisp@gmail.com>
2025-04-05 20:16:06 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './modules/material/material.module';
+import { LayoutComponent } from './components/layout/layout/layout.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
+import { PreloaderComponent } from './components/layout/preloader/preloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavigationComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
